3eb4ee29a87055047fc8a9e203dfe339f9caf599
Spoorthigopal <spoorthigopal123@gmail.com>
2025-12-14 15:12:28 +0530

    enu agilla

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
-
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app=express();
+connectDB();
 app.use("/api/notes",notesRoutes);
-
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+const PORT=process.env.PORT ||5000;
+app.listen(PORT,()=>{
+    console.log("Server is running on port :",PORT);
 });
 
